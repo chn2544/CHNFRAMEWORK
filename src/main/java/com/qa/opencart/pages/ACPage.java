a452c1212b256becd56d86f1182ebc321ee48258
Gogte <Chinmay.Gogte@allscripts.com>
2022-06-13 20:31:42 +0530

    Update Post PR Review

--- src/main/java/com/qa/opencart/pages/ACPage.java
+++ src/main/java/com/qa/opencart/pages/ACPage.java
@@ -1,10 +1,19 @@
 package com.qa.opencart.pages;
 
+import org.openqa.selenium.By;
+
 public class ACPage {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Acc Page Purpose");
+		final By check=By.cssSelector("ChinLoc");  // as written in static main method
+		
+		
+	}
+	public void clickcheck()
+	{
+		System.out.println("Clicking on button");
 	}
 
 }
