791c915eb1a9ac3af1c604e5a4c734e2ab2ea62d
Gogte <Chinmay.Gogte@allscripts.com>
2022-06-14 14:21:32 +0530

    adding v1 for targetrelease

---
+++ src/main/java/com/qa/opencart/pages/TagRelease.java
@@ -0,0 +1,12 @@
+package com.qa.opencart.pages;
+
+public class TagRelease {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("V1");
+
+	}
+
+}
