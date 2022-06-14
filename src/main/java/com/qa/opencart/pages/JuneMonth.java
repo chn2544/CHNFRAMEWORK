15859157537699b2aabe0c2277c011d69acde598
Chinmay Gogte <107382292+chn2544@users.noreply.github.com>
2022-06-14 10:20:07 +0530

    Create JuneMonth.java

---
+++ src/main/java/com/qa/opencart/pages/JuneMonth.java
@@ -0,0 +1,10 @@
+package com.qa.opencart.pages;
+
+public class JuneMonth {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			// Successfull Class in Git
+	}
+
+}
