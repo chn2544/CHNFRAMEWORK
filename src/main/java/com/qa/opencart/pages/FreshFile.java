15bd2b1d56e0b896f2059d8599e313840391b15c
Gogte <Chinmay.Gogte@allscripts.com>
2022-06-17 08:56:27 +0530

    Is all good

---
+++ src/main/java/com/qa/opencart/pages/FreshFile.java
@@ -0,0 +1,12 @@
+package com.qa.opencart.pages;
+
+public class FreshFile {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// Again checking how to revert the code
+		
+		System.out.println("Today is 17th June");
+	}
+
+}
