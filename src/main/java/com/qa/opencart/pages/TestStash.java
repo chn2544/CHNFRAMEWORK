6e3a708ab654d89f213e8e2aa680a36f9f7f6422
Gogte <Chinmay.Gogte@allscripts.com>
2022-06-17 09:40:14 +0530

    Inserting TestStash Class

---
+++ src/main/java/com/qa/opencart/pages/TestStash.java
@@ -0,0 +1,14 @@
+package com.qa.opencart.pages;
+
+public class TestStash {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// Checking Stash Memory Concept
+		
+		int a=200;
+		
+		
+	}
+
+}
