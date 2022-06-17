b705a4edd01328a7fae21e815232e6b24a996291
Gogte <Chinmay.Gogte@allscripts.com>
2022-06-17 08:53:07 +0530

    Adding code which is to be reverted

---
+++ src/main/java/com/qa/opencart/pages/ReverseCodeConcept.java
@@ -0,0 +1,11 @@
+package com.qa.opencart.pages;
+
+public class ReverseCodeConcept {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// This class is written to check Reverting code which is pushed or commited or staged
+		System.out.println("Checking reveritng of code");
+	}
+
+}
